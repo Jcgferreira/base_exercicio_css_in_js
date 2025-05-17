6ce2f2f0d8cfa3eeb629c2f1eba60527c0c91744
Jcgferreira <jcgferreira1220@outlook.com>
2025-05-17 16:47:25 -0300

    Resolving merge conflicts

--- src/components/FormVagas/styles.ts
+++ src/components/FormVagas/styles.ts
@@ -3,17 +3,29 @@ import styled from 'styled-components'
 export const StyledForm = styled.form.attrs({})<
   React.FormHTMLAttributes<HTMLFormElement>
 >`
-  display: flex;
-  gap: 1rem;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: ${(props) => props.theme.corSecundaria};
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
 `
 
 export const BtnPesquisar = styled.button.attrs({})<
   React.ButtonHTMLAttributes<HTMLButtonElement>
 >`
-  background: #007bff;
-  color: white;
-  padding: 0.5rem 1rem;
-  border: none;
-  border-radius: 4px;
+  background-color: ${(props) => props.theme.corPrincipal};
+  border: 1px solid ${(props) => props.theme.corPrincipal};
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: ${(props) => props.theme.corSecundaria};
+  margin-left: 8px;
   cursor: pointer;
+`
+export const Input = styled.input.attrs({})<
+  React.InputHTMLAttributes<HTMLInputElement>
+>`
+  padding: 0 16px;
+  outline-color: ${(props) => props.theme.corPrincipal};
 `
